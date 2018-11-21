6ed407be38710f82feb695af53db1662277a03b0
Luke Griffiths <wlgriffiths@gmail.com>
2018-11-21 13:08:21 -0700

    [feat] select output directory
    Adds the ability to run the scripts from elesewhere within FS.
    
    User has the option to output keys to their current working directory.

--- create_root-certificate-authority.bash
+++ create_root-certificate-authority.bash
@@ -2,13 +2,30 @@
 
 # thanks: https://alexanderzeitler.com/articles/Fixing-Chrome-missing_subjectAltName-selfsigned-cert-openssl/
 
-openssl genrsa -des3 -out ~/.ssl/rootCA.key 2048
+echo "Where to output files?"
+echo "1.  ~/.ssl/"
+echo "2.  current directory"
+
+output_dir=0
+
+read output_selection
+
+if [ "$output_selection" -eq 1 ]; then
+  output_dir="~/.ssl"
+elif [ $output_selection -eq 2 ]; then
+  output_dir=`pwd`
+else
+  echo "you must enter 1 or 2"
+  exit 1
+fi
+
+openssl genrsa -des3 -out "${output_dir}/rootCA.key" 2048
 
 openssl req \
   -x509 \
   -new \
   -nodes \
-  -key ~/.ssl/rootCA.key \
+  -key "${output_dir}/rootCA.key" \
   -sha256 \
   -days 1024 \
-  -out ~/.ssl/rootCA.pem
+  -out "${output_dir}/rootCA.pem"
--- create_self-signed-certificate.bash
+++ create_self-signed-certificate.bash
@@ -2,20 +2,40 @@
 
 # thanks: https://alexanderzeitler.com/articles/Fixing-Chrome-missing_subjectAltName-selfsigned-cert-openssl/
 
+echo "Where to output files?"
+echo "1.  ~/.ssl/"
+echo "2.  current directory"
+
+output_dir=0
+
+read output_selection
+
+if [ "$output_selection" -eq 1 ]; then
+  output_dir="~/.ssl"
+elif [ $output_selection -eq 2 ]; then
+  output_dir=`pwd`
+else
+  echo "you must enter 1 or 2"
+  exit 1
+fi
+
+# thanks: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 openssl req -new \
   -sha256 \
   -nodes \
-  -out ~/.ssl/SAN-friendly_server.csr \
+  -out "${output_dir}/SAN-friendly_server.csr" \
   -newkey rsa:2048 \
-  -keyout ~/.ssl/SAN-friendly_server.key \
-  -config <( cat SAN-friendly_server.csr.cnf )
+  -keyout "${output_dir}/SAN-friendly_server.key" \
+  -config <( cat "${script_dir}/SAN-friendly_server.csr.cnf" )
 
 openssl x509 -req \
-  -in ~/.ssl/SAN-friendly_server.csr \
-  -CA ~/.ssl/rootCA.pem \
-  -CAkey ~/.ssl/rootCA.key \
+  -in "${output_dir}/SAN-friendly_server.csr" \
+  -CA "${output_dir}/rootCA.pem" \
+  -CAkey "${output_dir}/rootCA.key" \
   -CAcreateserial \
-  -out ~/.ssl/SAN-friendly_server.crt \
+  -out "${output_dir}/SAN-friendly_server.crt" \
   -days 500 \
   -sha256 \
-  -extfile v3.ext
+  -extfile "${script_dir}/v3.ext"
